669715c2e0f56c7fd4ab49d5da55d6e93c8ab530
Hanna Madsen <89hanmad@mk.se>
2023-08-14 15:16:13 +0200

    REC-9 senaste

--- Models/ReceptModel.cs
+++ Models/ReceptModel.cs
@@ -1,5 +1,10 @@
-namespace ReceptBank.Models;
+namespace ReceptBank.Domain;
 public class Recept
 {
-public string? Maträtt { get; set; }
+public string Maträtt { get; set; }
+public Recept(string maträtt)
+{
+Maträtt = maträtt;
+
+}
 }
