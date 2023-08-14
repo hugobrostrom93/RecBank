dd54faa21ba4e984fcce7416fb8e813194b69fce
Hanna Madsen <89hanmad@mk.se>
2023-08-14 14:41:04 +0200

    REC-9 basic layout view

---
+++ Controllers/ReceptController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+namespace ReceptBank.Controllers;
+public class ReceptController : Controller
+{
+public IActionResult Recept ()
+{
+return View();
+}
+public IActionResult Sök ()
+{
+return View();
+}
+
+}
