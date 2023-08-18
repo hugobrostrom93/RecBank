33daaf7425785c7a49de69fdeb3ab176c85bf29a
Hanna Madsen <89hanmad@mk.se>
2023-08-18 11:13:55 +0200

    REC-10 olika saker

--- Application/ReceptDTO.cs
+++ Application/ReceptDTO.cs
@@ -2,6 +2,7 @@ namespace ReceptBank.ApplicationServices;
 
 public class ReceptDTO
 {
+    public int ReceptId {get; set;}
     public string? Name { get; set; }
     public string Ingredients { get; set; }
 }
